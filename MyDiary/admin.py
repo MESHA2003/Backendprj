79463ce499cf8a43a29d5058cdd9f3c8751cf70c
MESHA <bitam11-083@suza.ac.tz>
2024-12-23 12:08:17 +0300

    third

--- MyDiary/admin.py
+++ MyDiary/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-# Create your views hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.
+# Create your views here.
 admin.site.register(DiaryEntry)
 admin.site.register(UserSetting)
 admin.site.register(Tag)
